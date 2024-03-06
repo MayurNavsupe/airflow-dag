182d53a122e3c1505b907b1f4cd2908ca0269a44
MayurNavsupe <mayurnavsupe@gmail.com>
2024-03-06 10:47:53 +0530

    Create hello-world.py

---
+++ hello-world.py
@@ -0,0 +1,30 @@
+#datetime
+from datetime import timedelta, datetime
+
+# The DAG object
+from airflow import DAG
+
+# Operators
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
+
+# initializing the default arguments
+default_args = {
+		'owner': 'Ranga',
+		'start_date': datetime(2022, 3, 4),
+		'retries': 3,
+		'retry_delay': timedelta(minutes=5)
+}
+
+# Instantiate a DAG object
+hello_world_dag = DAG('hello_world_dag',
+		default_args=default_args,
+		description='Hello World DAG',
+		schedule_interval='* * * * *', 
+		catchup=False,
+		tags=['example, helloworld']
+)
+
+# python callable function
+def print_hello():
+		return 'Hello World!'
