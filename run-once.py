727b6d8cd070ecd78228b716a2100cda809f4403
MayurNavsupe <mayurnavsupe@gmail.com>
2024-03-16 16:08:47 +0530

    Create run-once.py

---
+++ run-once.py
@@ -0,0 +1,29 @@
+#datetime
+from datetime import timedelta, datetime
+
+# The DAG object
+from airflow import DAG
+
+# Operators
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
+
+# initializing the default arguments
+default_args = {
+		'owner': 'airflow',
+		'schedule_interval='@once',
+}
+
+# Instantiate a DAG object
+hello_world_dag = DAG('hello_world_dag',
+		default_args=default_args,
+		description='Hello World DAG',
+		schedule_interval='* * * * *', 
+		catchup=False,
+		tags=['example, helloworld'],
+		access_control={"qa1": {"can_read", "can_edit", "can_delete"}},
+)
+
+# python callable function
+def print_hello():
+		return 'Hello World!'
