904279987b1519bf752333b49b1a18bfdebffdae
MayurNavsupe <mayurnavsupe@gmail.com>
2024-03-16 16:13:12 +0530

    Update run-once.py

--- run-once.py
+++ run-once.py
@@ -15,7 +15,7 @@ default_args = {
 }
 
 # Instantiate a DAG object
-hello_world_dag = DAG('hello_world_dag',
+hello_world_dag = DAG('run_once_dag',
 		default_args=default_args,
 		description='Hello World DAG',
 		schedule_interval='* * * * *', 
